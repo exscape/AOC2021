c88ccdcfd9955f0196ecca2192160d62c60817bf
Thomas Backman <serenity@exscape.org>
2021-12-03 09:48:39 +0100

    Add Day 2 solutions.

---
+++ src/day2.ts
@@ -0,0 +1,37 @@
+import { Solution } from './solution.js';
+import { readLines } from './common.js';
+
+class Day2 implements Solution {
+    answer() {
+        readLines("data/day2.txt", (data) => {
+            // Index 0 = Part 1 solution, index 1 = Part 2 solution
+            let position = [0, 0];
+            let depth = [0, 0];
+            let aim = 0;
+            for (let line of data.split("\r\n")) {
+                let [command, arg] = line.split(" ");
+                let value = parseInt(arg);
+                switch (command) {
+                    case "up":
+                        depth[0] -= value;
+                        aim -= value;
+                        break;
+                    case "down":
+                        depth[0] += value;
+                        aim += value;
+                        break;
+                    case "forward":
+                        position[0] += value;
+                        position[1] += value;
+                        depth[1] += aim * value;
+                        break;
+                }
+            }
+
+            console.log(`Day 2 Part 1: ${position[0] * depth[0]} (pos=${position[0]}, depth=${depth[0]})`);
+            console.log(`Day 2 Part 2: ${position[1] * depth[1]} (pos=${position[1]}, depth=${depth[1]}, aim=${aim})`);
+        })
+    }
+}
+
+export { Day2 }
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
 import { Day1 } from './day1.js';
+import { Day2 } from './day2.js';
 
 (new Day1).answer();
+(new Day2).answer();
