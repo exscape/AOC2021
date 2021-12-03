637100d51a4795a856980aaa2a9a9e6402378092
Thomas Backman <serenity@exscape.org>
2021-12-03 13:45:30 +0100

    Day 3 Part 2

--- src/day3.ts
+++ src/day3.ts
@@ -1,27 +1,46 @@
 import { Solution } from './solution.js';
 import { readLines } from './common.js';
 
+function mostCommon(lines: string[], position: number) {
+    let ones = 0;
+    for (let line of lines) {
+        if (line[position] == "1") {
+            ones++;
+        }
+    }
+
+    return (ones >= lines.length / 2) ? 1 : 0;
+}
+
 class Day3 implements Solution {
     answer() {
         readLines("data/day3.txt", (data) => {
-            let lines = data.split("\r\n").map((s) => s.split("").reverse().join(""))
-                                          .filter((s) => s.length > 0);
-            let gammaRate = 0;
+            let lines = data.split("\r\n").filter((s) => s.length > 0);
             
-            for (let i = 0; i < lines[0].length; i++) {
-                let ones = 0;
-                for (let line of lines) {
-                    if (line[i] == "1") {
-                        ones++;
-                    }
-                }
-
-                if (ones > lines.length / 2)
-                    gammaRate |= (1 << i);
+            // Part 1
+            let gammaRate = 0;
+            for (let i = 0; i < 12; i++) {
+                if (mostCommon(lines, i) == 1)
+                    gammaRate |= (1 << (11-i));
             }
 
             let epsilonRate = ~gammaRate & 0xfff;
-            console.log(`Day 3 Part 1: ${gammaRate * epsilonRate} (gamma ${gammaRate}, epsilon ${epsilonRate})`)
+            console.log(`Day 3 Part 1: ${gammaRate * epsilonRate}`)
+
+            // Part 2
+            let filteredLines = [...lines];
+            for (let i = 0; i < 12 && filteredLines.length > 1; i++) {
+                filteredLines = filteredLines.filter((s) => parseInt(s[i]) == mostCommon(filteredLines, i));
+            }
+            let oxygenGeneratorRating = parseInt(filteredLines[0], 2)
+
+            filteredLines = [...lines];
+            for (let i = 0; i < 12 && filteredLines.length > 1; i++) {
+                filteredLines = filteredLines.filter((s) => parseInt(s[i]) != mostCommon(filteredLines, i));
+            }
+            let CO2ScrubberRating = parseInt(filteredLines[0], 2)
+
+            console.log(`Day 3 Part 2: ${oxygenGeneratorRating! * CO2ScrubberRating!}`);
         });
     }
 }
