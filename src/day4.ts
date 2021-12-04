d632bbac6ee7766ec178c0360c016a2b7f7ae437
Thomas Backman <serenity@exscape.org>
2021-12-04 11:15:54 +0100

    Day 4 Part 1

---
+++ src/day4.ts
@@ -0,0 +1,110 @@
+import { Solution } from './solution.js';
+import { readFile, arraySum } from './common.js';
+
+class Square {
+    value: number;
+    drawn: boolean;
+
+    constructor(value: number, drawn: boolean) {
+        this.value = value
+        this.drawn = drawn
+    }
+}
+
+class Board {
+    board: Square[][];
+
+    constructor(lines: string[]) {
+        this.board = []
+        for (let line of lines) {
+            let row = line.trim()
+                          .split(new RegExp("\\s+"))
+                          .map(s => new Square(parseInt(s), false));
+            this.board.push(row);
+        }
+    }
+
+    mark(draw: number) {
+        for (let row = 0; row < 5; row++) {
+            for (let col = 0; col < 5; col++) {
+                if (this.board[row][col].value == draw)
+                    this.board[row][col].drawn = true // Can we break here? Not sure if duplicates are allowed or not.
+            }
+        }
+    }
+
+    hasBingo(): boolean {
+        if (this.board.some(row => this.lineHasBingo(row)))
+            return true;
+        if (this.boardColumns().some(col => this.lineHasBingo(col)))
+            return true;
+
+        return false;
+    }
+
+    score(lastDraw: number): number {
+        // Find the sum of all unmarked numbers, then multiply by lastDraw
+        return lastDraw * arraySum(this.board
+                                       .flat()
+                                       .filter(sq => !sq.drawn)
+                                       .map(sq => sq.value));
+    }
+
+    lineHasBingo(line: Square[]): boolean {
+        return line.every(square => square.drawn);
+    }
+
+    boardColumns(): Square[][] {
+        // Transpose the matrix to make the columns into rows (and vice versa)
+        return this.board[0].map((_,i) => this.board.map(x => x[i]))
+    }
+
+    print() {
+        for (let row = 0; row < 5; row++) {
+            for (let col = 0; col < 5; col++) {
+                if (this.board[row][col].drawn)
+                    process.stdout.write('\u001b[' + 41 + 'm');
+
+                process.stdout.write(`${this.board[row][col].value}`);
+
+                if (this.board[row][col].drawn)
+                    process.stdout.write('\u001b[0m');
+               process.stdout.write(" ");
+            }
+            process.stdout.write("\n");
+        }
+    }
+}
+
+class Day4 implements Solution {
+    answer() {
+        readFile("data/day4.txt", (data) => {
+            let [numbers, ...board_data] = data.split("\r\n\r\n");
+            console.log(`Numbers: ${numbers}`);
+            let boards = [];
+
+            for (let data of board_data) {
+                let board = new Board(data.split("\r\n"));
+                boards.push(board);
+                board.print();
+                console.log("\n"); 
+            }
+
+            outer:
+            for (let drawnStr of numbers.split(",")) {
+                for (let board of boards) {
+                    let draw = parseInt(drawnStr);
+                    board.mark(draw);
+                    if (board.hasBingo()) {
+                        console.log("We have a winner! Board:");
+                        board.print();
+                        console.log(`Score is: ${board.score(draw)}`);
+                        break outer
+                    }
+                }
+            }
+        });
+    }
+}
+
+export { Day4 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { Day1 } from './day1.js';
 import { Day2 } from './day2.js';
 import { Day3 } from './day3.js';
+import { Day4 } from './day4.js';
 
 (new Day1).answer();
 (new Day2).answer();
 (new Day3).answer();
+(new Day4).answer();
