d254108928e76d5f088e7457aa039a0856b11613
Thomas Backman <serenity@exscape.org>
2021-12-04 12:33:57 +0100

    Rewrite Day 4 solution a bit

--- src/day4.ts
+++ src/day4.ts
@@ -6,18 +6,20 @@ class Square {
     drawn: boolean;
 
     constructor(value: number, drawn: boolean) {
-        this.value = value
-        this.drawn = drawn
+        this.value = value;
+        this.drawn = drawn;
     }
 }
 
 class Board {
     board: Square[][];
     hasWon: boolean;
+    lastDraw: number;
 
     constructor(lines: string[]) {
-        this.board = []
-        this.hasWon = false
+        this.board = [];
+        this.hasWon = false;
+        this.lastDraw = -1;
 
         for (let line of lines) {
             let row = line.trim()
@@ -28,10 +30,12 @@ class Board {
     }
 
     mark(draw: number) {
+        this.lastDraw = draw;
+
         for (let row = 0; row < 5; row++) {
             for (let col = 0; col < 5; col++) {
                 if (this.board[row][col].value == draw)
-                    this.board[row][col].drawn = true
+                    this.board[row][col].drawn = true;
             }
         }
     }
@@ -45,9 +49,9 @@ class Board {
         return false;
     }
 
-    score(lastDraw: number): number {
+    score(): number {
         // Find the sum of all unmarked numbers, then multiply by lastDraw
-        return lastDraw * arraySum(this.board
+        return this.lastDraw * arraySum(this.board
                                        .flat()
                                        .filter(sq => !sq.drawn)
                                        .map(sq => sq.value));
@@ -59,25 +63,7 @@ class Board {
 
     boardColumns(): Square[][] {
         // Transpose the matrix to make the columns into rows (and vice versa)
-        return this.board[0].map((_,i) => this.board.map(x => x[i]))
-    }
-
-    print(draw?: number) {
-        for (let row = 0; row < 5; row++) {
-            for (let col = 0; col < 5; col++) {
-                if (this.board[row][col].drawn)
-                    process.stdout.write('\u001b[' + 41 + 'm');
-
-                process.stdout.write(`${this.board[row][col].value}`);
-
-                if (this.board[row][col].drawn)
-                    process.stdout.write('\u001b[0m');
-               process.stdout.write(" ");
-            }
-            process.stdout.write("\n");
-        }
-        if (draw !== undefined)
-            console.log(`Score is: ${this.score(draw)}\n`);
+        return this.board[0].map((_,i) => this.board.map(x => x[i]));
     }
 }
 
@@ -85,43 +71,32 @@ class Day4 implements Solution {
     answer() {
         readFile("data/day4.txt", (data) => {
             let [numbers, ...board_data] = data.split("\r\n\r\n");
-            let boards = [];
+            let boards: Board[] = [];
 
             for (let data of board_data) {
                 boards.push(new Board(data.split("\r\n")));
             }
 
-            let winners = new Array<[Board, number]>();
-
+            outer:
             for (let drawnStr of numbers.split(",")) {
                 let draw = parseInt(drawnStr);
 
                 for (let board of boards) {
-                    if (board.hasWon)
-                        continue;
-
                     board.mark(draw);
 
-                    if (board.hasBingo())
-                        winners.push([board, draw]);
+                    if (board.hasBingo()) {
+                        if (boards.length == board_data.length) {
+                            // True if this is the first winner, as no boards have been removed from the game yet
+                            console.log(`Day 4 Part 1: ${board.score()}`);
+                        }
+                        else if (boards.length == 1) {
+                            console.log(`Day 4 Part 2: ${board.score()}`);
+                            break outer
+                        }
+                        boards = boards.filter((b: Board) => b != board);
+                    }
                 }
-
-                if (boards.every(b => b.hasWon))
-                        break
             }
-
-            console.log("---------- DAY 4 ----------");
-
-            // Part 1
-            console.log("First winner:");
-            let [board, draw] = winners[0];
-            board.print(draw);
-
-            // Part 2
-            [board, draw] = winners[winners.length - 1];
-            console.log("Last winner:");
-            board.print(draw);
-            console.log("---------------------------");
         });
     }
 }
