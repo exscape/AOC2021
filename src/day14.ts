89e7c666a5cd8842be8a607acddf06cbf120cb72
Thomas Backman <serenity@exscape.org>
2021-12-14 22:13:26 +0100

    Day 14 Part 2. Rewrite to non-naive, optimized solution

--- src/day14.ts
+++ src/day14.ts
@@ -1,70 +1,64 @@
 import { Solution } from './solution.js';
 import { readFile } from './common.js';
 
-class InsertionRule {
-    match: string;
-    insert: string;
-
-    constructor(match: string, insert: string) {
-        this.match = match;
-        this.insert = insert;
-    }
+function addToCount(map: Map<string, number>, key: string, amount: number) {
+    if (map.get(key) == undefined) map.set(key, amount);
+    else map.set(key, map.get(key)! + amount);
 }
 
-function applyRules(template: string, insertionRules: InsertionRule[]) {
-    // Make a list of insertion points first, then insert changes.
-    let insertionPoints: [number, string][] = [];
-    for (let i = 0; i < template.length - 1; i++) {
-        for (let rule of insertionRules) {
-            if (template.slice(i, i+2) == rule.match)
-                insertionPoints.push([i+1, rule.insert]);
+function createPolymer(iterations: number, state: Map<string, number>, insertionRules: Map<string, string>, letterCountMap: Map<string, number>) {
+    for (let i = 0; i < iterations; i++) {
+        let newState: Map<string, number> = new Map(state);
+        for (let [pair, count] of state.entries()) {
+            const char = insertionRules.get(pair);
+            const [left, right] = [pair[0] + char, char + pair[1]];
+
+            addToCount(letterCountMap, char!, count);
+            addToCount(newState, left, count);
+            addToCount(newState, right, count);
+            addToCount(newState, pair, -count)
         }
+        state = newState;
     }
 
-    let lastIndex = insertionPoints[insertionPoints.length - 1][0];
-    let output: string[] = [];
-
-    let prevIndex = 0;
-    for (let point of insertionPoints) {
-        output.push(template.slice(prevIndex, point[0]));
-        output.push(point[1]);
-        prevIndex = point[0];
-    }
-    output.push(template.slice(lastIndex, template.length));
-
-    return output.join('');
+    return letterCountMap;
 }
 
-function frequencyMap(s: string) {
-    let map: Record<string, number> = {};
-    for (let i = 0; i < s.length; i++) {
-        if (map[s[i]] == undefined)
-            map[s[i]] = 1;
-        else
-            map[s[i]]++;
+function generatePairs(template: string) {
+    let pairs: Map<string, number> = new Map();
+    for (let i = 0; i < template.length - 1; i++) {
+        addToCount(pairs, template.slice(i, i+2), 1);
     }
 
-    return map;
+    return pairs;
 }
 
 export class Day14 implements Solution {
     answer() {
         readFile("data/day14.txt", (data) => {
-            let [template, insertionRulesData] = data.split("\r\n\r\n");
-            let insertionRules = insertionRulesData.split(/\r?\n/).map(line => {
-                let [match, insert] = line.split(" -> ");
-                return new InsertionRule(match, insert);
-            });
+            const [template, insertionRulesData] = data.split("\r\n\r\n");
 
-            let output = template;
-            for (let i = 0; i < 10; i++) {
-                output = applyRules(output, insertionRules);
-            }
+            const insertionRules = new Map(
+                insertionRulesData.split(/\r?\n/).map(line => {
+                    const [match, insert] = line.split(" -> ");
+                    return [match, insert]
+                })
+            );
 
-            let map = frequencyMap(output);
-            let min = Math.min(...Object.values(map));
-            let max = Math.max(...Object.values(map));
-            console.log(`Day 14 Part 1: ${max - min}`);
+            const initialState = generatePairs(template);
+
+            const initialLetterCountMap: Map<string, number> = new Map();
+            template.split('').forEach(letter => addToCount(initialLetterCountMap, letter, 1));
+
+            // Part 1
+            let letterCountMap = createPolymer(10, initialState, insertionRules, new Map(initialLetterCountMap));
+            let values = [...letterCountMap.values()].sort((a,b) => b-a);
+            console.log(`Day 14 Part 1: ${values[0] - values[values.length - 1]}`);
+
+            // Part 2
+            letterCountMap = createPolymer(40, initialState, insertionRules, new Map(initialLetterCountMap));
+            values = [...letterCountMap.values()].sort((a,b) => b-a);
+            console.log(`Day 14 Part 2: ${values[0] - values[values.length - 1]}`);
         });
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -7,18 +7,21 @@ import { Day5 } from './day5.js';
 import { Day6 } from './day6.js';
 import { Day7 } from './day7.js';
 import { Day8_Rewrite } from './day8_rewrite.js';
+import { Day8_TESTING } from './day8_TESTING.js';
 import { Day9 } from './day9.js';
 import { Day10 } from './day10.js';
 import { Day11 } from './day11.js';
 import { Day12 } from './day12.js';
 import { Day13 } from './day13.js';
+import { Day14 } from './day14.js';
 
 // Saves 20 microseconds the next time it's run -- JIT I suppose?
 let perf = new Perf();
 perf.end(false);
 
 let days = [new Day1(), new Day2(), new Day3(), new Day4(), new Day5(), new Day6(), new Day7(),
-            new Day8_Rewrite(), new Day9(), new Day10(), new Day11(), new Day12(), new Day13()];
+            new Day8_Rewrite(), new Day9(), new Day10(), new Day11(), new Day12(), new Day13(),
+            new Day14()];
 
 /*for (let day of days) {
     day.answer();
