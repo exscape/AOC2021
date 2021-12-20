b0574d5023537734a99a15c792c888b083e82fa8
Thomas Backman <serenity@exscape.org>
2021-12-20 11:34:12 +0100

    Day 20 Part 1

---
+++ src/day20.ts
@@ -0,0 +1,75 @@
+import { Solution } from './solution.js';
+import { readFile, GenericGrid, Coordinate } from './common.js';
+
+class Grid extends GenericGrid<boolean> {
+    outOfBoundsPixelValue = false;
+
+    initialize(data: string[], oob: boolean): Grid {
+        for (let line of data) {
+            this._squares.push(line.split('').map(c => c == '#'));
+        }
+
+        this.outOfBoundsPixelValue = oob;
+        return this;
+    }
+
+    // The one in common.ts is dissimilar enough that a copy/paste/modify is cleaner than changing the original -- I did try that first...
+    // The main difference being that one only select adjacent squares/pixels, not the active one as well. Plus the ordering is incorrect for this problem.
+    adjacentPixels(coord: Coordinate) {
+        const isOutOfBounds = (c: Coordinate) => (c.x < 0 || c.x >= this.width() || c.y < 0 || c.y >= this.height());
+        let pixels = [];
+        for (let y = coord.y - 1; y <= coord.y + 1; y++) {
+            for (let x = coord.x - 1; x <= coord.x + 1; x++) {
+                let c = new Coordinate(x, y);
+                pixels.push(isOutOfBounds(c) ? this.outOfBoundsPixelValue : this.at(c));
+            }
+        }
+
+        return pixels;
+    }
+
+    enhance(imageEnhancement: string): Grid {
+        let outputData = [];
+        for (let y = -1; y < this.height() + 1; y++) {
+            let rowChars = [];
+            for (let x = -1; x < this.width() + 1; x++) {
+                let coord = new Coordinate(x, y);
+                let lookupValue = parseInt(this.adjacentPixels(coord).flatMap(v => v ? "1" : "0").join(''), 2);
+                rowChars.push(imageEnhancement[lookupValue]);
+            }
+            outputData.push(rowChars.join(''));
+        }
+
+        // All infinite pixels outside the used grid will be either all zeroes or all ones.
+        // In the former case, they should be replaced with imageEnhancement[0], and the latter imageEnhancement[511 = 0b111111111].
+        let oob;
+        if (this.outOfBoundsPixelValue == false)
+            oob = imageEnhancement[0];
+        else
+            oob = imageEnhancement[511];
+
+        let output = new Grid().initialize(outputData, oob == "#");
+        console.log(`Original grid ${this.width()}x${this.height()}, new grid ${output.width()}x${output.height()}`);
+        return output;
+    }
+
+    litPixelCount() {
+        return [...this.squares()]
+               .filter(p => p === true)
+               .length;
+    }
+}
+
+export class Day20 implements Solution {
+    answer() {
+        readFile("data/day20.txt", (data) => {
+            let [imageEnhancement, imageData] = data.split("\r\n\r\n");
+            let image = new Grid().initialize(imageData.split("\r\n"), false);
+            for (let i = 0; i < 2; i++) {
+                image = image.enhance(imageEnhancement);
+            }
+
+            console.log(`Day 20 Part 1: ${image.litPixelCount()}`);
+        });
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,7 @@ import { Day14 } from './day14.js';
 import { Day15 } from './day15.js';
 import { Day16 } from './day16.js';
 import { Day17 } from './day17.js';
+import { Day20 } from './day20.js';
 
 // Saves 20 microseconds the next time it's run -- JIT I suppose?
 let perf = new Perf();
@@ -24,7 +25,7 @@ perf.end(false);
 
 let days = [new Day1(), new Day2(), new Day3(), new Day4(), new Day5(), new Day6(), new Day7(),
             new Day8_Rewrite(), new Day9(), new Day10(), new Day11(), new Day12(), new Day13(),
-            new Day14(), new Day15(), new Day16(), new Day17()];
+            new Day14(), new Day15(), new Day16(), new Day17(), new Day20()];
 
 /*for (let day of days) {
     day.answer();
