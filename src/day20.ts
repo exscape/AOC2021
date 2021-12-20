de205dab2dd21c3762e257de47540baf8a80385d
Thomas Backman <serenity@exscape.org>
2021-12-21 00:02:47 +0100

    Refactor + optimize away a few 100 ms

--- src/day20.ts
+++ src/day20.ts
@@ -1,10 +1,10 @@
 import { Solution } from './solution.js';
 import { readFile, GenericGrid, Coordinate, Perf } from './common.js';
 
-class Grid extends GenericGrid<boolean> {
+class InfiniteGrid extends GenericGrid<boolean> {
     outOfBoundsPixelValue = false;
 
-    initialize(data: string[], oob: boolean): Grid {
+    initialize(data: string[], oob: boolean): InfiniteGrid {
         for (let line of data) {
             this._squares.push(line.split('').map(c => c == '#'));
         }
@@ -13,29 +13,37 @@ class Grid extends GenericGrid<boolean> {
         return this;
     }
 
+    isOutOfBounds = (c: Coordinate) => (c.x < 0 || c.x >= this.width() || c.y < 0 || c.y >= this.height());
+    at = (coord: Coordinate) => this.isOutOfBounds(coord) ? this.outOfBoundsPixelValue : this._squares[coord.y][coord.x];
+
     // The one in GenericGrid is dissimilar enough that a copy/paste/modify is cleaner than changing the original -- I did try that first...
     // The main difference being that the GenericGrid one only select adjacent squares/pixels, not the active one as well.
     // In addition, the ordering is incorrect for this problem.
     adjacentPixels(coord: Coordinate) {
-        const isOutOfBounds = (c: Coordinate) => (c.x < 0 || c.x >= this.width() || c.y < 0 || c.y >= this.height());
         let pixels = [];
         for (let y = coord.y - 1; y <= coord.y + 1; y++) {
             for (let x = coord.x - 1; x <= coord.x + 1; x++) {
                 let c = new Coordinate(x, y);
-                pixels.push(isOutOfBounds(c) ? this.outOfBoundsPixelValue : this.at(c));
+                pixels.push(this.at(c));
             }
         }
 
         return pixels;
     }
 
-    enhance(imageEnhancement: string): Grid {
+    enhance(imageEnhancement: string): InfiniteGrid {
         let outputData = [];
         for (let y = -1; y < this.height() + 1; y++) {
             let rowChars = [];
             for (let x = -1; x < this.width() + 1; x++) {
                 let coord = new Coordinate(x, y);
-                let lookupValue = parseInt(this.adjacentPixels(coord).flatMap(v => v ? "1" : "0").join(''), 2);
+
+                let lookupValue = 0;
+                for (let [i, v] of this.adjacentPixels(coord).flat().entries()) {
+                    if (v)
+                        lookupValue |= (1 << (8-i));
+                }
+
                 rowChars.push(imageEnhancement[lookupValue]);
             }
             outputData.push(rowChars.join(''));
@@ -49,7 +57,7 @@ class Grid extends GenericGrid<boolean> {
         else
             oob = imageEnhancement[511];
 
-        let output = new Grid().initialize(outputData, oob == "#");
+        let output = new InfiniteGrid().initialize(outputData, oob == "#");
         return output;
     }
 
@@ -60,7 +68,7 @@ class Grid extends GenericGrid<boolean> {
     }
 }
 
-function repeatedEnhance(image: Grid, count: number, imageEnhancement: string): Grid {
+function repeatedEnhance(image: InfiniteGrid, count: number, imageEnhancement: string): InfiniteGrid {
     for (let i = 0; i < count; i++) {
         image = image.enhance(imageEnhancement);
     }
@@ -74,7 +82,7 @@ export class Day20 implements Solution {
             let perf = new Perf();
             let [imageEnhancement, imageData] = data.split("\r\n\r\n");
 
-            let image = new Grid().initialize(imageData.split("\r\n"), false);
+            let image = new InfiniteGrid().initialize(imageData.split("\r\n"), false);
 
             image = repeatedEnhance(image, 2, imageEnhancement);
             console.log(`Day 20 Part 1: ${image.litPixelCount()}`);
